b51ea764898917f028d6bcf88e3f018a693b3f39
Sungoh Park <spark449062@gmail.com>
2021-07-22 12:40:12 -0600

    commit tree 50b84e77f423857b74fecfcbd21dd767ffa21634

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ] ;
 
+function superbowlWin(inputRecord) {
+    function testWin(gameInstance) {
+        // console.log(gameInstance);
+        return gameInstance.result === "W"
+    }
+    if (inputRecord.find(testWin) === undefined) {
+        return undefined;
+    }
+    else return inputRecord.find(testWin).year;
+}
+
+console.log(superbowlWin(record));
